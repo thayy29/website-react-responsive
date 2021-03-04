81bef9dafc6be72ccf99b5b4df7bda9cae9b3c32
Thay <thayana.cs@gmail.com>
2021-03-04 17:45:54 -0300

    feat/finished-cards-section

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import '../../App.css';
+import Cards from '../Cards';
 import MainSection from '../MainSection';
 
 function Home() {
   return (
     <>
       <MainSection />
+      <Cards />
     </>
   );
 }
